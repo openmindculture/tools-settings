41230197ae81fc66b554dd74fad394fef2c71ec7
Ingo Steinke <ingosteinke@gmx.de>
2021-05-16 11:52:20 +0200

    Create docker-armageddon.sh

---
+++ tools/docker-armageddon.sh
@@ -0,0 +1,9 @@
+# removecontainers
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+# purge everything else
+docker network prune -f
+docker rmi -f $(docker images --filter dangling=true -qa)
+docker volume rm $(docker volume ls --filter dangling=true -q)
+docker rmi -f $(docker images -qa)
