458d558ccad16d172d2c14943933f9e0d92280e2
Ingo Steinke <ingosteinke@gmx.de>
2022-02-12 10:46:05 +0100

    add high resolution example

--- tools/scan-to-pdf.sh
+++ tools/scan-to-pdf.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # scan images
+
+# low resolution gray scale
 scanimage --format jpeg --resolution 300  --output img1.jpg
 scanimage --format jpeg --resolution 300  --output img2.jpg
+
+# colorful higher resolution
+scanimage --format jpeg --resolution 600 --mode Color --output img3.jpg
 
 # convert to pdf using imagemagick
 convert img*.jpg img.pdf
